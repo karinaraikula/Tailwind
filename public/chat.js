533d0cf063db243dea7d311e7f195e5f00c55ecf
karinaraikula <karina.raikula@metropolia.fi>
2022-09-13 18:40:52 +0300

    first commit chat layout

---
+++ public/chat.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Server URL below must point to your server, localhost works for local development/testing
+const socket = io("http://localhost:3000/");
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inp = document.getElementById("m");
+  socket.emit("chat message", inp.value);
+  inp.value = "";
+});
+
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
+  item.classList.add(
+    "py-2",
+    "px-3",
+    "bg-secondary-dark",
+    "rounded-lg",
+    "text-white",
+    "w-fit",
+    "m-5"
+  );
+
+  item.innerHTML = msg;
+  document.getElementById("messages").appendChild(item);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "primary-dark": "#201f20",
+      "primary-light": "#d9d9d9",
+      "primary-color": "#d2c8fa",
+      "secondary-dark": "#35323a",
+    },
+  },
+  plugins: [],
+};
